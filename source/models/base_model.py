25ec7798ac0dd75b96ccc50e4873a11a807f4aaa
Xritsos <kobecris24@yahoo.gr>
2024-05-11 20:36:24 +0300

    added the bade model

---
+++ source/models/base_model.py
@@ -0,0 +1,37 @@
+"""This is an implementation of the model suggested on 
+https://towardsdatascience.com/how-to-generate-music-using-a-lstm-neural-network-in-keras-68786834d4c5
+"""
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import BatchNormalization as BatchNorm
+
+
+def create_base_model(network_input, n_vocab):
+    model = Sequential()
+    model.add(LSTM(512, 
+                   input_shape=(network_input.shape[1], network_input.shape[2]), 
+                   recurrent_dropout=0.3, 
+                   return_sequences=True))
+    
+    model.add(LSTM(512, return_sequences=True, recurrent_dropout=0.3,))
+    model.add(LSTM(512))
+    model.add(BatchNorm())
+    model.add(Dropout(0.3))
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(BatchNorm())
+    model.add(Dropout(0.3))
+    model.add(Dense(n_vocab))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+    return model
+    
