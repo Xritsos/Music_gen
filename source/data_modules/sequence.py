764d893e9e01dec5341a63f6eaaad12395a7e97d
Xritsos <kobecris24@yahoo.gr>
2024-05-11 21:31:23 +0300

    module to create sequences for training

---
+++ source/data_modules/sequence.py
@@ -0,0 +1,39 @@
+"""This module creates the sequences that are the inputs of the model."""
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+import numpy as np
+from tensorflow.keras import utils
+
+
+def prepare_sequences(notes, n_vocab):
+    sequence_length = 100
+
+    # get all pitch names
+    pitchnames = sorted(set(item for item in notes))
+
+    # create a dictionary to map pitches to integers
+    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
+
+    network_input = []
+    network_output = []
+
+    # create input sequences and the corresponding outputs
+    for i in range(0, len(notes) - sequence_length, 1):
+        sequence_in = notes[i:i + sequence_length]
+        sequence_out = notes[i + sequence_length]
+        network_input.append([note_to_int[char] for char in sequence_in])
+        network_output.append(note_to_int[sequence_out])
+
+    n_patterns = len(network_input)
+
+    # reshape the input into a format compatible with LSTM layers
+    network_input = np.reshape(network_input, (n_patterns, sequence_length, 1))
+    
+    # normalize input
+    network_input = network_input / float(n_vocab)
+
+    network_output = utils.to_categorical(network_output)
+
+    return (network_input, network_output)
