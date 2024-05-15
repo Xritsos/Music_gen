f264e5b61bf98cc50447641703f2b490b5701337
Xritsos <kobecris24@yahoo.gr>
2024-05-15 20:06:02 +0300

    read parameters from file

--- tests/test_training.py
+++ tests/test_training.py
@@ -5,6 +5,7 @@ https://towardsdatascience.com/how-to-generate-music-using-a-lstm-neural-network
 import os
 import sys
 import pickle
+import pandas as pd
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
@@ -16,7 +17,9 @@ from source.data_modules import sequence
 from source.models import base_model
 
 
-def train():
+def train(test_id):
+    tf.random.set_seed(11)
+    
     data_path = './data/notes'
     
     try:
@@ -31,14 +34,29 @@ def train():
         print()
         print("Notes loaded successfully !")
         
+    df = pd.read_csv('./tests.csv')
+    
+    n_epochs = int(df['epochs'][test_id-1])
+    batch_size = int(df['batch_size'][test_id-1])
+    learning_rate = float(df['learning_rate'][test_id-1])
+    sequence_length = int(df['sequence_length'][test_id-1])
+    weight_decay = float(df['weight_decay'][test_id-1])
+    optim = df['optimizer'][test_id-1]
+        
     # get amount of pitch names
     n_vocab = len(set(notes))
 
-    network_input, network_output = sequence.prepare_sequences(notes, n_vocab)
+    network_input, network_output = sequence.prepare_sequences(notes, n_vocab, sequence_length)
 
     model = base_model.get_base_model(network_input, n_vocab)
     
-    checkpoint_filepath = './model_ckpts/test_model.keras'
+    if optim == 'adam':
+        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate,
+                                             weight_decay=weight_decay)
+    
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+    
+    checkpoint_filepath = f'./model_ckpts/{test_id}_model.keras'
     checkpoint = ModelCheckpoint(checkpoint_filepath,
                                  monitor='loss', 
                                  verbose=0, 
@@ -47,15 +65,29 @@ def train():
     
     callbacks_list = [checkpoint]
 
-    model.fit(network_input, network_output, 
-              epochs=2, 
-              batch_size=128,
-              callbacks=callbacks_list)
+    history = model.fit(network_input, network_output, 
+                        epochs=n_epochs, 
+                        batch_size=batch_size, 
+                        callbacks=callbacks_list)
+    
+    min_loss = min(history.history['loss'])
+    
+    df.at[test_id-1, 'loss'] = min_loss
+    df.to_csv('./tests.csv', index=False)
+    
+    epochs = [i for i in range(1, n_epochs+1)]
+    to_file = {'epoch': epochs, 'loss': history.history['loss']}
+    
+    log_df = pd.DataFrame(to_file)
+    log_df.to_csv(f'./logs/{test_id}.csv', index=False)
     
     
 if __name__ == "__main__":
     print(tf.config.list_physical_devices('GPU'))
+    
+    test_id = 1
+    
 
     with tf.device('/device:GPU:0'):
-        train()
+        train(test_id)
 
