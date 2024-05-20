1fc43217276966936548dec9db6863d639ba3bcf
Xritsos <kobecris@yahoo.gr>
2024-05-20 20:17:52 +0300

    test_base_model renamed to test_models

--- tests/main.py
+++ tests/main.py
@@ -3,14 +3,13 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 
 from test_training import train
-from test_base_model import generate
+from test_models import generate
 
 
 if __name__ == "__main__":
     
     print(tf.config.list_physical_devices('GPU'))
 
-    # test_id = 13
     for test_id in [29, 30, 31, 32, 33, 34]:
         with tf.device('/device:GPU:0'):
             train(test_id)
