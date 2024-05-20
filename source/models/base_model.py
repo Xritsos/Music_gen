c9346b34d8a3e80daaa28e2dba6ca704eaec6466
Xritsos <kobecris@yahoo.gr>
2024-05-20 21:30:03 +0300

    added glorot seed

--- source/models/base_model.py
+++ source/models/base_model.py
@@ -12,6 +12,7 @@ from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import BatchNormalization as BatchNorm
+from tensorflow.keras import initializers
 
 
 def get_base_model(network_input, n_vocab, drop_factor):
@@ -21,17 +22,18 @@ def get_base_model(network_input, n_vocab, drop_factor):
     model.add(LSTM(512, 
                    input_shape=(network_input.shape[1], network_input.shape[2]), 
                    recurrent_dropout=drop_factor, 
-                   return_sequences=True))
+                   return_sequences=True,
+                   kernel_initializer=initializers.glorot_uniform(seed=0)))
     
-    model.add(LSTM(512, return_sequences=True, recurrent_dropout=drop_factor))
-    model.add(LSTM(512))
+    model.add(LSTM(512, return_sequences=True, recurrent_dropout=drop_factor, kernel_initializer=initializers.glorot_uniform(seed=0)))
+    model.add(LSTM(512, kernel_initializer=initializers.glorot_uniform(seed=0)))
     model.add(BatchNorm())
     model.add(Dropout(drop_factor))
-    model.add(Dense(256))
+    model.add(Dense(256, kernel_initializer=initializers.glorot_uniform(seed=0)))
     model.add(Activation('relu'))
     model.add(BatchNorm())
     model.add(Dropout(drop_factor))
-    model.add(Dense(n_vocab))
+    model.add(Dense(n_vocab, kernel_initializer=initializers.glorot_uniform(seed=0)))
     model.add(Activation('softmax'))
 
     return model
