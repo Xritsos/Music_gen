9277646bb2e25e2708804e34fea0f353d565a10b
Xritsos <kobecris24@yahoo.gr>
2024-05-22 23:25:11 +0300

    new tests

--- tests/main.py
+++ tests/main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     
     print(tf.config.list_physical_devices('GPU'))
 
-    for test_id in [62]:
+    for test_id in [63, 64, 65, 66]:
         with tf.device('/device:GPU:0'):
             train(test_id)
         
